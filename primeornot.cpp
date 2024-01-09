a49f1605d19d98d7bd043fafb3d8f0a54ef54d4e
Soham Das <138379203+sohamdas1105@users.noreply.github.com>
2024-01-09 11:39:21 +0530

    primeornot.cpp

--- primeornot.cpp
+++ primeornot.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+   int howmuch;
+   cin>>howmuch;
+   for( int n=2;n<=howmuch;n++){
+       cout<< n <<endl ;
+       if(n%2==0){
+           cout<<"notprime "<<endl;
+       }
+       else{
+           cout<<"prime "<<endl;
+       }
+   }
+    return 0;
+}
